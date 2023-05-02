299409c93c8d2443b10421a35332fb5a87cd1a20
aseelbdoor <aseel.bdoor2001@gmail.com>
2023-05-02 19:56:21 +0000

    whitebord

---
+++ python/reverse.py
@@ -0,0 +1,16 @@
+#Aseel Bdoor
+def reverseArray(list):
+    newlist=[]
+    for i in range(len(list)-1,-1,-1):
+        newlist.append(list[i])
+    return newlist
+
+#main
+list1=[1, 2, 3, 4, 5, 6]
+list2=[89, 2354, 3546, 23, 10, -923, 823, -12]
+list3=[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
+        47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
+        107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163,
+        167, 173, 179, 181, 191, 193, 197, 199]
+
+print(reverseArray(list3))
