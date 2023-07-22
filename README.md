83475a9e1ce5c62f4a09e715ede652687d5eabef
aseelbdoor <aseel.bdoor2001@gmail.com>
2023-07-22 21:32:39 +0000

    tests

--- README.md
+++ README.md
@@ -19,3 +19,4 @@ You can find in this repo multiple function created  with python to solve proble
 |Insertion Sort|[Insertion Sort](./python/sorting/insertion/insertion_sort.md)|
 |Merge Sort|[Merge Sort](./python//sorting/merge/merge_sort.md)|
 |Sorting Objects|[Sorting Objects](./python/sorting/Comparisons/sort.md)|
+|Hash Table|[hashtable](./python/hashtable/hashtable.md)|
--- python/sorting/insertion/insertion_sort.py
+++ python/sorting/insertion/insertion_sort.py
@@ -21,12 +21,6 @@ class Sort:
 
         Returns:
             None
-
-        Description:
-            This method takes a sorted list and a value as input.
-            It finds the correct position for the value in the sorted list, such that the list remains sorted.
-            The value is inserted into the correct position using the 'insert' method of the list object.
-            The insert operation modifies the 'sorted_list' in-place, and it does not return any value.
         """
         i = len(sorted_list) - 1
         while i >= 0 and value < sorted_list[i]:
@@ -42,13 +36,6 @@ class Sort:
 
         Returns:
             list: The sorted list.
-
-        Description:
-            This method takes an input list and performs the insertion sort algorithm to sort the elements in ascending order.
-            It initializes a 'sorted_list' with the first element from the input list.
-            Then, it iterates over the remaining elements in the input list, and for each element, it calls the 'insert' method
-            to insert it into the correct position in the 'sorted_list'.
-            The method returns the final sorted list.
         """
         sorted_list = [input_list[0]]
         for i in range(1, len(input_list)):
