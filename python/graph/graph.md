77b4de4e533fe0c9d0ecd5d7cf70e46b7d55f7e5
aseelbdoor <aseel.bdoor2001@gmail.com>
2023-08-12 20:55:04 +0000

    graph

---
+++ python/graph/graph.md
@@ -0,0 +1,47 @@
+# Graph
+
+**Required for .venv** `pip install pytest`
+## Vertix
+Is used to represent individual vertices or nodes in a graph. A vertex typically contains a value that represents the data associated with it. The constructor (__init__) takes a single argument value and initializes the vertex's value attribute. The __str__ method is not defined in this version, but it's common to define it to provide a human-readable representation of the vertex when printed.
+
+## Edge
+ Is used to represent edges between vertices in a graph. An edge connects two vertices and may have an associated weight, which could represent a distance, cost, or any other relevant value. The constructor (__init__) takes two arguments: vertex, which represents the connected vertex, and weight, which is an optional parameter representing the weight of the edge. The constructor initializes the edge's weight and vertex attributes.
+
+## Graph
+  - **add_vertix**
+    - Arguments: value
+    - Returns: The added vertex
+    - Add a vertex to the graph
+
+  - **add_edge**
+    - Arguments: 2 vertices
+    - Returns: nothing
+    - Adds a new edge between two vertices in the graph
+    - If specified, assign a weight to the edge
+    - Both vertices should already be in the Graph
+
+  - **get_vertices**
+    - Arguments: none
+    - Returns: all of the vertices in the graph
+    - collection (set, list, or similar)
+    - Empty collection returned if there are no vertices
+
+  - **get_neighbors**
+    - Arguments: vertex
+    - Returns: collection of edges connected to the 
+      given vertex
+    - Include the weight of the connection in the          returned collection
+    - Empty collection returned if there are no vertices
+
+  - **breadth_first**
+    - Arguments: start_vertex
+    - Returns:  A list containing the values of the vertices visited during the traversal in the order they were traversed.
+
+## How to run the code 
+`python3 python/graph/graph.py`
+
+## Test
+`pytest`
+
+
+[Open the code](./graph.py)
---
+++ python/graph/graph.py
@@ -0,0 +1,133 @@
+from collections import deque 
+# we spill it 'd-e-c-k'
+
+class Queue:
+
+    def __init__(self):
+        self.dq = deque()
+
+    def enqueue(self, value):
+        self.dq.append(value)
+
+    def dequeue(self):
+        return self.dq.popleft()
+
+    def __len__(self):
+        return len(self.dq)
+
+
+
+class Vertix:
+
+    def __init__(self, value):
+
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class Edge:
+
+    def __init__(self, vertix, weight=0):
+        self.weight = weight
+        self.vertix = vertix
+
+    def __str__(self):
+        return f"{self.vertix},{self.weight}"
+
+class Graph:
+
+    def __init__(self):
+        self.__adj_list = {}
+      
+    def add_vertix(self,value):
+      '''
+      Arguments: value
+      Returns: The added vertex
+      Add a vertex to the graph
+      '''
+  
+      vertix = Vertix(value)
+      self.__adj_list[vertix] = []
+      return vertix
+
+
+    def add_edge(self, start_vertix, end_vertix=None, weight=0):
+        '''
+        Arguments: 2 vertices to be connected by the                     edge, weight (optional)
+        Returns: nothing
+        Adds a new edge between two vertices in the graph
+        If specified, assign a weight to the edge
+        Both vertices should already be in the Graph
+        '''
+        if start_vertix not in self.__adj_list:
+            raise KeyError("Start vertex is not found")
+        if end_vertix not in self.__adj_list:
+            raise KeyError("End vertex is not found")
+        edge1 = Edge(end_vertix, weight)
+        edge2 = Edge(start_vertix)
+        self.__adj_list[start_vertix].append(edge1)
+        self.__adj_list[end_vertix].append(edge2)
+
+  
+    def get_vertices(self):
+      '''
+        Arguments: none
+        Returns all of the vertices in the graph as a  
+        collection (set, list, or similar)
+        Empty collection returned if there are no vertices
+      '''
+  
+      return self.__adj_list.keys()
+
+  
+    def size(self):
+      return len(self.__adj_list)
+  
+    def get_neighbors(self,vertix):
+      '''
+      get neighbors
+      A rguments: vertex
+      Returns a collection of edges connected to the 
+      given vertex
+      Include the weight of the connection in the                      returned collection
+      Empty collection returned if there are no vertices
+      '''
+      # return self.__adj_list[vertix]
+      return self.__adj_list.get(vertix, [])
+  
+    def breadth_first(self,start_vertix):
+        """
+        Perform a breadth-first traversal of the graph starting from the specified vertex.
+
+        Args:
+            start_vertex (Vertex): The vertex from which the traversal should begin.
+
+        Returns:
+            list: A list containing the values of the vertices visited during the traversal
+                  in the order they were traversed.
+        """
+    
+        result = []
+        visted = set()
+        q = Queue()
+
+        q.enqueue(start_vertix)
+        visted.add(start_vertix)
+
+        while len(q):
+            current_vertix = q.dequeue()
+
+            result.append(current_vertix.value)
+
+            neighbors =self.get_neighbors(current_vertix)
+
+            for edge in neighbors:
+                neighbor = edge.vertix
+                if neighbor not in visted:
+                    q.enqueue(neighbor)
+                    visted.add(neighbor)
+
+        return result
+    
